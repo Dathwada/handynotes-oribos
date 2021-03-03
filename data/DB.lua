0ce9b064735bd229fc8564cb3b74fb132c140928
Dathwada <Dathwada@users.noreply.github.com>
2021-03-03 18:48:23 +0100

    Add two new mailboxes

--- data/DB.lua
+++ data/DB.lua
@@ -82,6 +82,7 @@ DB.points = {
     [58693031] = { icon="banker", npc=156479 },
     [58102771] = { icon="banker", npc=156479 },
     [65233051] = { icon="greatvault", label=greatvault },
+    [58163602] = { icon="mail", label=mailbox },
     [65203600] = { icon="guildvault", label=guildvault },
 
     -- THE IDYLLIA
@@ -111,6 +112,7 @@ DB.points = {
     [34645648] = { icon="reforge", npc=164096 },
 
     [23324895] = { icon="portaltrainer", npc=176186, class="MAGE" },
+    [30645226] = { icon="mail", label=mailbox },
 
     [20835477] = { icon="portal", label=PtoOG, note=Durotar, faction="Horde", quest=60151 },
     [20894567] = { icon="portal", label=PtoSW, note=ElwynnForest, faction="Alliance", quest=60151 },
